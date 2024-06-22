68354ea9728f82c2e2e8447e793a446559dae87e
abd1rahmann <abdi123rahman@outlook.com>
2024-06-22 02:45:20 +0200

    ny kod för test

---
+++ Bilsimulator.Tests/Tests/DriverTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.Service.DriverServices;
+
+namespace Bilsimulator.Tests.Tests
+{
+    [TestClass]
+    public class DriverTests
+    {
+        private IDriverService driverService;
+        private Driver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driverService = new DriverService();
+            driver = new Driver();
+        }
+
+        [TestMethod]
+        public void Rest_ShouldReduceTiredness()
+        {
+            driver.Tiredness = 5;
+            driverService.Rest(driver);
+            Assert.AreEqual(3, driver.Tiredness);
+        }
+
+        [TestMethod]
+        public void CheckFatigue_ShouldWarnWhenTired()
+        {
+            driver.Tiredness = Driver.WarningTiredness;
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                driverService.CheckFatigue(driver);
+                var result = sw.ToString().Trim();
+                Assert.AreEqual("Föraren är väldigt trött. Det är dags för en rast!", result);
+            }
+        }
+
+        [TestMethod]
+        public void CheckFatigue_ShouldAlertWhenExtremelyTired()
+        {
+            driver.Tiredness = Driver.MaxTiredness;
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                driverService.CheckFatigue(driver);
+                var result = sw.ToString().Trim();
+                Assert.AreEqual("Föraren är extremt trött. Det är farligt att köra. Ta en lång rast!", result);
+            }
+        }
+    }
+}
